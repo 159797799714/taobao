e906314f76db1f55a4431504b3ff2117aab5425a
15979779714 <1142414499@qq.com>
2018-10-17 11:44:56 +0800

    购物车修改初始版

--- js/detail.js
+++ js/detail.js
@@ -1,25 +1,65 @@
-var add_char = (function(){
+
+if(localStorage.length == 0){
+	var arr = [];
+	var id = 0;
+}
+else{
+    var arr = [localStorage.arr];
+    var id = localStorage.id ;
+}
+var add_char = (function () {
 	var add = document.querySelector('.add_car');
 	var no = document.querySelector('.no');
+	var cancel = document.querySelector('.cancel');
 	var shop_main = document.querySelector('.shop_main');
-	var lesser =document.querySelector('.lesser');
-	var adder =document.querySelector('.adder');
+	var lesser = document.querySelector('.lesser');
+	var adder = document.querySelector('.adder');
 	var val_num = document.querySelector('.val_num');
-	add.onclick = function(){
+	var sure = document.querySelector('.sure');
+	var price = document.querySelector('.price');
+	var img = document.querySelector('#photo1');
+	var name = document.querySelector('.name');
+	var src = img.src;
+	var a = price.innerHTML.split('.');
+	var b = (a[0].match(/\d+/g));
+	var [p] = b;
+	var pri = Number(p);
+	add.onclick = function () {
+		val_num.value = 1;
 		shop_main.style.display = 'block';
-		no.onclick = function(){
+		no.onclick = function () {
 			shop_main.style.display = 'none';
 		}
-		lesser.onclick =function(){
-			if(val_num.value > 1){
+		cancel.onclick = function () {
+			shop_main.style.display = 'none';
+		}
+		lesser.onclick = function () {
+			if (val_num.value > 1) {
 				val_num.value--;
 			}
-			else{
+			else {
 				val_num.value = 1;
 			}
 		}
-		adder.onclick = function(){
+		adder.onclick = function () {
 			val_num.value++;
 		}
+		sure.onclick = function () {
+			var data ={
+				price:pri,
+				num:val_num.value,
+				src:src,
+				name:name.innerHTML,
+				id:id
+			}
+			id ++;
+			data = JSON.stringify(data);
+			arr.push(data);
+			localStorage.arr = arr;
+			shop_main.style.display = 'none';
+			console.log(localStorage.arr);
+		}
 	}
+
+
 }())
--- js/shop.js
+++ js/shop.js
@@ -1,10 +1,233 @@
-/* 
-* @Author: anchen
-* @Date:   2018-10-12 08:47:51
-* @Last Modified by:   anchen
-* @Last Modified time: 2018-10-12 10:32:58
-*/
 
-$(document).ready(function(){
-    
-});
+var arr2 = [];
+var item_li = document.querySelector('.item_li');
+var getLocalStorage = (function () {
+    // console.log(localStorage.arr);
+    var a = (localStorage.arr).split(',{');
+    for (var i = 0; i < a.length; i++) {
+        if (i >= 1) {
+            a[i] = '{' + a[i];
+        }
+        b = JSON.parse(a[i]);
+        arr2.push(b);
+        var img_innerHTML = '<img src="' + arr2[i].src + '">';
+        var name_innerHTML = arr2[i].name;
+        var price_innerHTML = '￥' + arr2[i].price;
+        var all_innerHTML = '￥' + Number(arr2[i].num) * Number(arr2[i].price);
+        var newUl = document.createElement('ul');
+        newUl.setAttribute("class", 'item_clear' + ' ' + i);
+        var str = `<li class="select"><i></i></li>
+        <li class="item_img">${img_innerHTML}</li>
+        <li class="item_name">${name_innerHTML}</li>
+        <li class="item_weight"></li>
+        <li class="item_price">${price_innerHTML}</li>
+        <li class="item_num">
+            <div class="num">
+                <button class="lesser">-</button>
+                <input class="val_num" type="text" value="1">
+                <button class="adder">+</button>
+            </div>
+        </li>
+        <li class="all_price">${all_innerHTML}</li>
+        <li class="item_control">
+            <a class="keep">加入收藏夹</a>
+            <a class="kill">删除</a>
+        </li>`;
+        newUl.innerHTML = str;
+        item_li.appendChild(newUl);
+        var num = document.querySelector('.val_num');
+        num.value = arr2[i].num;
+        // console.log(arr2[i].num)
+
+        // var lesser = document.querySelector('.lesser');
+        // var adder = document.querySelector('.adder');
+        // var kill = document.querySelector('.kill');
+        // lesser.onclick = function () {
+        //     if (num.value > 1) {
+        //         num.value--;
+
+        //     }
+        //     else {
+        //         num.value = 1;
+        //     }
+        //     // all_price.innerHTML = '￥' + Number(num.value) * Number(arr2.price);
+        //     // //通过判断结算是否为橙色来判断是否选择商品
+        //     // if (sum_pay.style.background == 'orangered') {
+        //     //     harmony.innerHTML = all_price.innerHTML;
+        //     //     sum.innerHTML = all_price.innerHTML;
+        //     // }
+        // }
+        // //添加
+        // adder.onclick = function () {
+        //     num.value++;
+        //     // all_price.innerHTML = '￥' + Number(num.value) * Number(arr2.price);
+        //     // if (sum_pay.style.background == 'orangered') {
+        //     //     harmony.innerHTML = all_price.innerHTML;
+        //     //     sum.innerHTML = all_price.innerHTML;
+        //     // }
+        // }
+        // kill.onclick = function () {
+        //                 newUl.remove();
+        //                 localStorage.clear();
+        //                 console.log('删除了DOM元素');
+        //                 // sum_pay.style.background = '#ccc';
+        //                 // settle.style.background = '#ccc';
+        //                 // harmony.innerHTML = '0.00';
+        //                 // sum.innerHTML = '0.00';
+        //                 // sel_sum.firstElementChild.innerHTML = '0';
+        //             }
+
+    }
+    var lesser = document.querySelector('.lesser');
+        var adder = document.querySelector('.adder');
+        var kill = document.querySelector('.kill');
+        lesser.onclick = function () {
+            if (num.value > 1) {
+                num.value--;
+
+            }
+            else {
+                num.value = 1;
+            }
+            // all_price.innerHTML = '￥' + Number(num.value) * Number(arr2.price);
+            // //通过判断结算是否为橙色来判断是否选择商品
+            // if (sum_pay.style.background == 'orangered') {
+            //     harmony.innerHTML = all_price.innerHTML;
+            //     sum.innerHTML = all_price.innerHTML;
+            // }
+        }
+        //添加
+        adder.onclick = function () {
+            num.value++;
+            // all_price.innerHTML = '￥' + Number(num.value) * Number(arr2.price);
+            // if (sum_pay.style.background == 'orangered') {
+            //     harmony.innerHTML = all_price.innerHTML;
+            //     sum.innerHTML = all_price.innerHTML;
+            // }
+        }
+        kill.onclick = function () {
+                        newUl.remove();
+                        localStorage.clear();
+                        console.log('删除了DOM元素');
+                        // sum_pay.style.background = '#ccc';
+                        // settle.style.background = '#ccc';
+                        // harmony.innerHTML = '0.00';
+                        // sum.innerHTML = '0.00';
+                        // sel_sum.firstElementChild.innerHTML = '0';
+                    }
+
+
+
+
+    // arr = localStorage.arr.split(',');
+    // arr2 = arr;
+    // console.log(arr2);
+    // for(var i in a){
+    //     console.log(i)
+    // }
+    // var main = document.querySelector('.main');
+    // var item_li = document.querySelector('.item_li');
+    // //判断详情页是点击传入数据到localSrorage
+    // if (a.length != 0) {
+    //     //在主页面下添加商品详情ul
+    //     var img_innerHTML = '<img src="' + a.src + '">';
+    //     var name_innerHTML = a.name;
+    //     var price_innerHTML = '￥' + a.price;
+    //     var all_innerHTML = '￥' + Number(a.num) * Number(a.price);
+    //     function addul() {
+    //         var newUl = document.createElement('ul');
+    //         newUl.setAttribute("class", 'item_clear');
+    //         var str = `<li class="select"><i></i></li>
+    //     <li class="item_img">${img_innerHTML}</li>
+    //     <li class="item_name">${name_innerHTML}</li>
+    //     <li class="item_weight"></li>
+    //     <li class="item_price">${price_innerHTML}</li>
+    //     <li class="item_num">
+    //         <div class="num">
+    //             <button class="lesser">-</button>
+    //             <input class="val_num" type="text" value="1">
+    //             <button class="adder">+</button>
+    //         </div>
+    //     </li>
+    //     <li class="all_price">${all_innerHTML}</li>
+    //     <li class="item_control">
+    //         <a class="keep">加入收藏夹</a>
+    //         <a class="kill">删除</a>
+    //     </li>`;
+    //         newUl.innerHTML = str;
+    //         item_li.appendChild(newUl);
+    //         var num = document.querySelector('.val_num');
+    //         num.value = a.num;
+    //         var childAll = item_li.children;
+    //         var item_clear = document.querySelector('.item_clear');
+    //         var item_select = document.querySelector('.select');
+    //         var val = document.querySelector('.val_num');
+    //         var kill = document.querySelector('.kill');
+    //         var all_price = document.querySelector('.all_price');
+    //         var item_control = document.querySelector('.item_control');
+    //         var lesser = document.querySelector('.lesser');
+    //         var adder = document.querySelector('.adder');
+    //         //减少，最小为1
+    //         lesser.onclick = function () {
+    //             if (val.value > 1) {
+    //                 val.value--;
+    //             }
+    //             else {
+    //                 val.value = 1;
+    //             }
+    //             all_price.innerHTML = '￥' + Number(val.value) * Number(a.price);
+    //             //通过判断结算是否为橙色来判断是否选择商品
+    //             if (sum_pay.style.background == 'orangered') {
+    //                 harmony.innerHTML = all_price.innerHTML;
+    //                 sum.innerHTML = all_price.innerHTML;
+    //             }
+    //         }
+    //         //添加
+    //         adder.onclick = function () {
+    //             val.value++;
+    //             all_price.innerHTML = '￥' + Number(val.value) * Number(a.price);
+    //             if (sum_pay.style.background == 'orangered') {
+    //                 harmony.innerHTML = all_price.innerHTML;
+    //                 sum.innerHTML = all_price.innerHTML;
+    //             }
+    //         }
+    //         //删除
+    //         kill.onclick = function () {
+    //             newUl.remove();
+    //             localStorage.clear();
+    //             console.log('删除了DOM元素');
+    //             sum_pay.style.background = '#ccc';
+    //             settle.style.background = '#ccc';
+    //             harmony.innerHTML = '0.00';
+    //             sum.innerHTML = '0.00';
+    //             sel_sum.firstElementChild.innerHTML = '0';
+    //         }
+    //         //点击选框
+    //         var sum_pay = document.querySelector('.sum_pay');
+    //         var settle = document.querySelector('.settle');
+    //         var harmony = document.querySelector('.harmony');
+    //         var sum = document.querySelector('.sum');
+    //         var sel_sum = document.querySelector('.sel_sum');
+    //         item_select.onclick = function () {
+    //             if (item_select.innerHTML == '<i></i>') {
+    //                 item_select.innerHTML = '<i>✔</i>';
+    //                 sum_pay.style.background = 'orangered';
+    //                 settle.style.background = 'orangered';
+    //                 harmony.innerHTML = all_price.innerHTML;
+    //                 sum.innerHTML = all_price.innerHTML;
+    //                 sel_sum.firstElementChild.innerHTML = '1';
+    //             }
+    //             else {
+    //                 item_select.innerHTML = '<i></i>';
+    //                 sum_pay.style.background = '#ccc';
+    //                 settle.style.background = '#ccc';
+    //                 harmony.innerHTML = '0.00';
+    //                 sum.innerHTML = '0.00';
+    //                 sel_sum.firstElementChild.innerHTML = '0';
+    //             }
+    //         }
+    //     }
+    //     addul();
+    // }
+
+}())
